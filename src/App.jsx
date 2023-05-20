d4e4cb792a2eba4a1b0325482d3ed72311f69aef
vicentemarciot27 <vicentemarciot27@gmail.com>
2023-05-20 16:38:44 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Resto from "./components/Resto";
+
+function App() {
+    return (
+        <>
+          oi
+          <Navbar></Navbar>
+          <Resto></Resto>
+        </>
+    );
+}
+
+export default App;
